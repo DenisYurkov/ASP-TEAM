c8078a93841493faf8b710d4728b200bb832d66e
CoffeeLED <James.unevich@gmail.com>
2021-01-26 23:21:52 +0300

    Auto stash before merge of "main" and "origin/main"

---
+++ Login.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/LoginNestedmasterPage.master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="WebApplication4.Login" %>
---
+++ LoginNestedmasterPage.master.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication4
+{
+    public partial class LoginNestedmasterPage : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Submit_Login_Click(object sender, EventArgs e)
+        {
+
+            string Email = "Admin@mail.com";
+            string Password = "admin123";  
+            
+            if (Email_Input_Box.Text == Email) {
+                if (Pass_Input_Box.Text == Password)
+                {
+                    Response.Redirect("/Default.aspx");
+                }
+                else {
+                    NotCorrect.Text = "Email or password is not correct";
+                }
+            }
+            else
+            {
+                NotCorrect.Text = "Email or password is not correct";
+            }
+        }
+    }
+}
